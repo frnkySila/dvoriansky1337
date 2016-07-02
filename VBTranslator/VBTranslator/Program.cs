86ac1081c22b869cb65de4e73ae8be2ecd12423d
Ilya Gulkov <isgulkov@edu.hse.ru>
2016-07-03 02:38:00 +0300

    Added a compiler C# project
    Finished Program.cs, empty VisualBasicMegaListener.cs

---
+++ VBTranslator/VBTranslator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace VBTranslator
+{
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string input = File.ReadAllText(args[0]);
+
+			var ms = new MemoryStream(Encoding.UTF8.GetBytes(input));
+			var lexer = new VisualBasicLexer(new AntlrInputStream(ms));
+
+			var tokens = new CommonTokenStream(lexer);
+
+			var parser = new VisualBasicParser(tokens);
+
+			var tree = parser.file();
+
+			var pastwk = new ParseTreeWalker();
+
+			pastwk.Walk(new VisualBasicMegaListener(), tree);
+		}
+	}
+}
---
+++ VBTranslator/VBTranslator/VisualBasicMegaListener.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VBTranslator
+{
+
+	public class VisualBasicMegaListener : VisualBasicBaseListener
+	{
+		
+	}
+}
+

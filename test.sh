c7561b8ef8fb6414a62917d1f10016ea77f408c4
Ilya Gulkov <isgulkov@edu.hse.ru>
2016-07-03 17:22:36 +0300

    Implemented test.sh and added the dworiansky test case

---
+++ test.sh
@@ -0,0 +1,10 @@
+mkdir -p ./test/compiled
+for in_file in $(ls -1 ./test | grep .vb)
+do
+	out_file="${in_file%.*}".cs
+	mono ./VBTranslator/VBTranslator/bin/Debug/VBTranslator.exe ./test/$in_file ./test/compiled/$out_file > /dev/null
+	mcs ./test/compiled/$out_file -reference:System.Windows.Forms > /dev/null
+	exe_file="${in_file%.*}".exe
+	mono ./test/compiled/$exe_file
+done
+# rm -rf ./test/compiled
---
+++ test/test.vb
@@ -0,0 +1,7 @@
+Dim i As Integer
+Dim a As Integer
+a=2
+For i=0 To 100 Step 2
+a=a + 5
+Next
+MsgBox "a=" & a

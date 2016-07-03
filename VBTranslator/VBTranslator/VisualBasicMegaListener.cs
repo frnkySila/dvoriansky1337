aaae06b0e2a4e52d967cd11e2d2fcc1fb6187abd
Ilya Gulkov <isgulkov@edu.hse.ru>
2016-07-03 04:26:35 +0300

    Implemented streamwriter stuff

--- VBTranslator/VBTranslator/VisualBasicMegaListener.cs
+++ VBTranslator/VBTranslator/VisualBasicMegaListener.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace VBTranslator
 {
@@ -6,10 +7,29 @@ namespace VBTranslator
 	public class VisualBasicMegaListener : VisualBasicBaseListener
 	{
 		readonly string OutputFilename;
+		StreamWriter OutputWriter;
 
 		public VisualBasicMegaListener(string outputFilename)
 		{
 			OutputFilename = outputFilename;
+
+			OutputWriter = new StreamWriter(File.Open(OutputFilename, FileMode.Create));
+			OutputWriter.AutoFlush = true;
+		}
+
+		public override void EnterFile(VisualBasicParser.FileContext context)
+		{
+			OutLine(context.GetText());
+		}
+
+		void OutLine(string s)
+		{
+			OutputWriter.Write(s + "\n");
+		}
+
+		void Out(string s)
+		{
+			OutputWriter.Write(s);
 		}
 	}
 }

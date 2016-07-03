d87930cf843c453444e3a5df386af3fed4f4ca24
Ilya Gulkov <isgulkov@edu.hse.ru>
2016-07-03 21:15:51 +0300

    Changed test.sh and make.sh closer to dvoriansky's vision

--- make.sh
+++ make.sh
@@ -1,2 +1,4 @@
 java -jar antlr-4.5.3-complete.jar VisualBasic.g4 -o VBTranslator/VBTranslator
 xbuild VBTranslator/VBTranslator.sln
+mkdir -p ./res
+cp ./VBTranslator/VBTranslator/bin/Debug/* ./res
--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ mkdir -p ./test/compiled
 for in_file in $(ls -1 ./test | grep .vb)
 do
 	out_file="${in_file%.*}".cs
-	mono ./VBTranslator/VBTranslator/bin/Debug/VBTranslator.exe ./test/$in_file ./test/compiled/$out_file > /dev/null
+	mono ./res/VBTranslator.exe ./test/$in_file ./test/compiled/$out_file > /dev/null
 	mcs ./test/compiled/$out_file -reference:System.Windows.Forms > /dev/null
 	exe_file="${in_file%.*}".exe
 	mono ./test/compiled/$exe_file
